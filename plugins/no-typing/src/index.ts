e6332f2a583d31344cb18288c12568d7ee66fd77
Daniel <davilareko@gmail.com>
2023-12-18 03:30:02 +0100

    Cleanup stuff from NoTyping

--- plugins/no-typing/src/index.ts
+++ plugins/no-typing/src/index.ts
@@ -1,9 +1,8 @@
 import { after, instead } from "@vendetta/patcher";
 import { findByName, findByProps } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
-import { FluxDispatcher, ReactNative } from "@vendetta/metro/common";
+import { FluxDispatcher } from "@vendetta/metro/common";
 import { findInReactTree } from "@vendetta/utils";
-import { React } from "@vendetta/metro/common";
 import { NoTypingIcon } from "./icon";
 
 const ChatInput = findByName("ChatInput");
@@ -13,30 +12,7 @@ const patches = ["startTyping"].map((k) => instead(k, Typing, ([channelId]) => {
     if (storage.noTyping_enabled) return;
     FluxDispatcher.dispatch({ type: "TYPING_START_LOCAL", channelId });
 }));
-// let i = 0;
 patches.push(after("render", ChatInput.prototype, (_, ret) => {
-    // console.log(ret);
-    // const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
-    // for (let index = 0; index < input.props.actions.length; index++) {
-    //     const element = input.props.actions[index];
-    //     console.log(element);
-    // }
-    // console.log(ret.props.children[2].props.children.props.children);
-    // const input = ret.props.children[2].props.children.props.children;
-    // for (let index = 0; index < input.length; index++) {
-    //     const element = input[index];
-    //     console.log(i, element);
-    // }
-    // console.log(input[1].props.children.props.children);
-    // console.log(input[1].props.children.props.children.length);
-    // const input2 = input[1].props.children.props.children as any[];
-    // for (let index = 0; index < input2.length; index++) {
-    //     const element = input2[index];
-    //     console.log(i, element);
-    // }
-    // input.splice(1, 0, React.createElement(NoTypingIcon));
-    // input.push(React.createElement(NoTypingIcon));
-    // i++;
     const actions = findInReactTree(
         ret,
         (x) => "forceAnimateButtons" in x.props && x.props.actions
